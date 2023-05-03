a46953768f05ea48e4690821e773b0f1727e3551
hheeseung <ssigu69@gmail.com>
2023-05-03 16:26:44 +0900

    fix: 연관동영상 API 연결 함수 오타 수정

--- src/components/RelatedList.jsx
+++ src/components/RelatedList.jsx
@@ -3,7 +3,9 @@ import { useQuery } from "@tanstack/react-query";
 
 export default function RelatedList({ videoId }) {
   const { data: relatedVideo } = useQuery(["relatedVideo"], () =>
-    fetch()`https://youtube.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${videoId}&maxResults=25&type=video&key=${process.env.REACT_APP_API_KEY}`
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${videoId}&maxResults=25&type=video&key=${process.env.REACT_APP_API_KEY}`
+    )
       .then((res) => res.json())
       .then((data) => data.items)
   );

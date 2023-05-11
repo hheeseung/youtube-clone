0e085f6c5ac8576df0595d5e80b6a69c1a286c48
hheeseung <ssigu69@gmail.com>
2023-05-11 14:27:52 +0900

    docs: Readme.md 수정

--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -1,12 +1,10 @@
 import { createContext, useContext } from "react";
 import Fetcher from "../services/fetcher";
 import FetcherClient from "../services/fetcherClient";
-import FakeFetcherClient from "../services/fakeFetcherClient";
 
 const ApiContext = createContext();
 
 const client = new FetcherClient();
-// const client = new FakeFetcherClient();
 const youtubeAPI = new Fetcher(client);
 
 export default function ApiProvider({ children }) {
